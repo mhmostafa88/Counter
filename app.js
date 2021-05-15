a955c69046854d79a5870411c38449e5d524187f
codingwizard88 <coding_wizard@outlook.com>
2021-05-15 18:42:07 +0200

    test commenting

---
+++ app.js
@@ -0,0 +1,34 @@
+//set initial counter
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click',function(e){
+        const styles = e.currentTarget.classList;
+        //determine the change to the count
+        if(styles.contains('decrease')){
+            count --;
+        }
+        else if(styles.contains('increase')){
+            count ++;
+        }
+        else{
+            count = 0;
+        }
+
+        //change colors
+        if(count > 0){
+            value.style.color = "green";
+        }
+        else if(count < 0){
+            value.style.color = "red";
+        }
+        else{
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    })
+})
